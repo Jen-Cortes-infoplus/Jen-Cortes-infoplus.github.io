17f952129b8327ea0e52d08a1143ddd510582bc3
Jen-Cortes-infoplus <jcortes@infopluscommerce.com>
2024-08-08 00:19:37 -0500

    Update bread-crumb.js

--- kb/bread-crumb.js
+++ kb/bread-crumb.js
@@ -3,20 +3,23 @@ function capitalizeFirstLetter(string) {
 }
 
 const url = window.location.href;
-var splitUrl = url.split('infoplus-wms')[1].split("/");
+var splitUrl = url.split('knowledge-base')[1].split("?")[0].split("/");
+var node = document.getElementsByClassName("breadcrumbs__nav")[0];
 
 for (var i = 0; i < splitUrl.length; i++) {
     if (splitUrl[i]) {
         var crumbText = splitUrl[i].replaceAll("-", " ");
-        var node = document.getElementsByClassName("breadcrumbs__nav")[0];
         var childNode = document.createElement("a");
 
-        childNode.innerHTML = capitalizeFirstLetter(crumbText);
+        console.log(i + " " + crumbText)
+        if (crumbText == "guides") {
+            childNode.innerHTML = '<i style="padding-right:8px;" class="fas icon fa-solid fa-book"></i>' + capitalizeFirstLetter(crumbText);
+        } else {
+            childNode.innerHTML = capitalizeFirstLetter(crumbText);
+        }
         childNode.href = url;
         childNode.className = "breadcrumbs__item";
 
         node.appendChild(childNode)
     }
 }
-
-console.log(url);
