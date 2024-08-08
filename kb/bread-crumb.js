e737dceeca274cae8a0ed28c64131f29966f979b
Jen-Cortes-infoplus <jcortes@infopluscommerce.com>
2024-08-08 02:44:29 -0500

    refactored and added headers

--- kb/bread-crumb.js
+++ kb/bread-crumb.js
@@ -1,30 +1,53 @@
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                      Html vars                                                     */
+/* ------------------------------------------------------------------------------------------------------------------ */
 var guidesIcon = '<i style="padding-right:13px;" class="fas icon fa-solid fa-book"></i>';
 var breadCrumbSvg = '<div class="bread-crumb-svg"><svg width="12" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M10.6817 1.6816l-4.5364 4-4.5364-3.9315" stroke="#d24566" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>';
 
-function capitalizeFirstLetter(string) {
-    return string[0].toUpperCase() + string.slice(1);
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                   Utils/Functions                                                  */
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+/* ------------------------------------------------- formatCrumbTxt ------------------------------------------------- */
+function formatCrumbTxt(url) {
+    var url2 = url.replaceAll("-", " ");
+    return url2[0].toUpperCase() + url2.slice(1);
 }
 
-const url = window.location.href;
-var splitUrl = url.split('knowledge-base')[1].split("?")[0].split("/");
+/* -------------------------------------------------- addNavBarNode ------------------------------------------------- */
+function addNavBarNode(url, navTxt) {
+    var childNode = document.createElement("a");
+
+    childNode.innerHTML = navTxt;
+    childNode.href = url;
+    childNode.className = "breadcrumbs__item";
+
+    node.appendChild(childNode);
+}
+
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                     Main Logic                                                     */
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+var noParamsUrl = window.location.href.split("?")[0];
+var splitUrl = noParamsUrl.split('knowledge-base')[1].split("/");
 var node = document.getElementsByClassName("breadcrumbs__nav")[0];
 node.innerHTML = "";
 
+addNavBarNode(noParamsUrl, guidesIcon + "Guides" + breadCrumbSvg);
+
 for (var i = 0; i < splitUrl.length; i++) {
     if (splitUrl[i]) {
-        var crumbText = capitalizeFirstLetter(splitUrl[i].replaceAll("-", " "));
-        var childNode = document.createElement("a");
+        var crumbText = formatCrumbTxt(splitUrl[i]);
 
-        if (crumbText == "Guides") {
-            childNode.innerHTML = guidesIcon + crumbText + breadCrumbSvg;
-        } else if (i == splitUrl.length - 1) {
-            childNode.innerHTML = crumbText;
+        if (i == splitUrl.length - 1) {
+            crumbText = crumbText;
         } else {
-            childNode.innerHTML = crumbText + breadCrumbSvg;
+            crumbText = crumbText + breadCrumbSvg;
         }
-        childNode.href = url;
-        childNode.className = "breadcrumbs__item";
 
-        node.appendChild(childNode)
+        addNavBarNode(noParamsUrl, crumbText);
     }
 }
