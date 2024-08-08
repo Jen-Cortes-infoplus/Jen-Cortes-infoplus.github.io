4e8a95e00a87a40cb082fc67a26b8800969c101f
Jen-Cortes-infoplus <jcortes@infopluscommerce.com>
2024-08-07 22:30:27 -0500

    Update bread-crumb.js

--- kb/bread-crumb.js
+++ kb/bread-crumb.js
@@ -8,7 +8,7 @@ var splitUrl = url.split('infoplus-wms')[1].split("/");
 for (var i = 0; i < splitUrl.length; i++) {
     if (splitUrl[i]) {
         var crumbText = splitUrl[i].replaceAll("-", " ");
-        var node = document.getElementsByTagName("nav")[0];
+        var node = document.getElementsByClassName("breadcrumbs__nav")[0];
         var childNode = document.createElement("a");
 
         childNode.innerHTML = capitalizeFirstLetter(crumbText);
